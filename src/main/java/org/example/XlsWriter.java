86ed84632dfd396aa24d49cd0754794e0473d649
https://github.com/nazarovmisha <nazarovmisha@inbox.ru>
2023-03-31 13:29:12 +0300

    Excel4

--- src/main/java/org/example/XlsWriter.java
+++ src/main/java/org/example/XlsWriter.java
@@ -1,4 +1,10 @@
 package org.example;
 
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 public class XlsWriter {
+   XSSFWorkbook workbook = new XSSFWorkbook();
+   XSSFSheet sheetUniversities = workbook.createSheet("Унивеситеты");
+
 }
